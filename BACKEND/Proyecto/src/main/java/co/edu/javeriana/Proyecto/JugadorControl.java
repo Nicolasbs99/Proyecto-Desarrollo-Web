e75b35b93548f46d68a49d3c39a438577ca0d4df
Nicolas Barragan <nbarragans@javeriana.edu.co>
2022-03-15 02:36:08 -0500

    Front arreglado

--- BACKEND/Proyecto/src/main/java/co/edu/javeriana/Proyecto/JugadorControl.java
+++ BACKEND/Proyecto/src/main/java/co/edu/javeriana/Proyecto/JugadorControl.java
@@ -6,11 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import co.edu.javeriana.Proyecto.Model.Jugador;
 import co.edu.javeriana.Proyecto.Model.JugadorRepo;
 
 @Controller
